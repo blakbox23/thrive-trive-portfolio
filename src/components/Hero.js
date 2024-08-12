e80fca3d206675dc3a6889bae05fadbcb02e7de8
EugineAgolla <agollaeugine@gmail.com>
2024-08-12 10:30:27 +0300

    hero changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="hero">
       <h1>Tech innovators Hub</h1>
-      <p>Find the best talent for your tech solutions!</p>
+      <p>Find the best talent for your tech solutions on Thrive Tribe</p>
       <a href="#" className="cta">
         Join us
       </a>
