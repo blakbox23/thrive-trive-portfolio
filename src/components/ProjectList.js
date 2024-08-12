b873f675f6004c2be8ce75eeefb437a46f2017b5
blakbox23 <boudii30@gmail.com>
2024-08-12 17:36:18 +0300

    Adds the Create functionality for projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,6 +3,11 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList() {
   const [projects, setProjects] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    category: "",
+  });
 
   useEffect(() => {
     fetch("http://localhost:4000/projects")
@@ -14,10 +19,31 @@ function ProjectList() {
   function handleDelete(id) {
     const remaining = projects.filter((project) => project.id !== id);
     setProjects(remaining);
-    
-    fetch(`http://localhost:4000/projects/${id}`, 
-    { method: "DELETE" })
-      .catch((error) => console.log(error));
+
+    fetch(`http://localhost:4000/projects/${id}`, { method: "DELETE" }).catch(
+      (error) => console.log(error)
+    );
+  }
+
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    setProjects([...projects, formData]);
+
+    fetch("http://localhost:4000/projects", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
   return (
@@ -32,6 +58,35 @@ function ProjectList() {
           onDelete={handleDelete}
         />
       ))}
+
+      <div className="form-section">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add name of the project"
+            name="name"
+            id="name"
+            onChange={handleChange}
+          />
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Add some description about the project"
+            rows="4"
+            cols="50"
+            onChange={handleChange}
+          />
+
+          <select name="category" onChange={handleChange} required>
+            <option value="">Select a category</option>
+            <option value="education">Education</option>
+            <option value="travel">Travel</option>
+            <option value="productivity">Productivity</option>
+          </select>
+
+          <input type="submit" value="Add project" />
+        </form>
+      </div>
     </div>
   );
 }
